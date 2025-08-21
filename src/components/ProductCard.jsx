47d2c44e1d7d9ee4d98a79982f19914eb48729f2
faris <faris.rahmat29@gmail.com>
2025-08-21 13:00:59 +0700

    memperbaiki stok product

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useCart } from '../context/CartContext.jsx';
 import { MinusIcon, PlusIcon } from './Icons.jsx';
 
